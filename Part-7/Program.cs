ea58a9e0c5cb16268eb51503c2b4a68b02b0fdb9
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-21 15:39:53 +0530

    Afternoon Session-6

---
+++ Part-7/Program.cs
@@ -0,0 +1,15 @@
+﻿//Expresion Lambda
+
+var numbers = new int[] { 2, 3, 4, 5, 6, 7, 8 };
+var count = numbers.Count(x => x == 5);
+Console.WriteLine(count);
+
+//Statement Lambda
+
+List<int> numbers2 = new List<int> { 2, 4, 3, 1, 2, 3 };
+var count2 = numbers.Count(x =>
+{
+    return x == 5;
+});
+Console.WriteLine(count2);
+
