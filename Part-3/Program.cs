580057488d73f8946621b0f21cca4ab9c9de7604
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-02-21 12:19:31 +0530

    Afternoon Session-1

---
+++ Part-3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Anonymous types
+var obj=new
+{
+    firstName="King",
+    lastName="Kochhar",
+    salary=12000,
+
+    address=new {streetName="Civil lines",city="Delhi"},
+    projects=new[]
+    {
+        new {projectName="Ecommerce",projectDuration="40 Hours"},
+        new {projectName="Admin Portal",projectDuration="40 Hours"},
+    }
+};
+
+Console.WriteLine(obj.firstName);
+Console.WriteLine(obj.address.streetName);
+
+foreach(var project in obj.projects)
+{
+    Console.WriteLine(project.projectName + " : " +project.projectDuration);
+    
+}
